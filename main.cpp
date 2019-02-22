e2f156c0f591df4ecda85d87ec46832fa2e12529
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-21 19:18:55 -0800

    Update files

--- main.cpp
+++ main.cpp
@@ -110,9 +110,9 @@ int main(){
        address_[4]  == '0' &&
        address_[5]  == '0' &&
        address_[6]  == '0' &&
-       address_[7]  == '0' &&
+       address_[7]  == '0'/* &&
        address_[8]  == '0' &&
-       address_[9]  == '0'){
+       address_[9]  == '0'*/){
       char* privateKeyString_ = PrivateKeyToString(privateKey_);
 
       std::ofstream outfile;
